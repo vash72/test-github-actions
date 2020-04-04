2457cb1de6842f8cd3e0d06fc79baf8ec02b2b99
Francesco Fortin <francesco.fortin@raintonic.com>
2020-04-04 10:01:20 +0200

    T2 Deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as faker from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-github-actions';
+  hello = 'Ciao belli';
+
+  ngOnInit(): void {
+
+    setInterval(() => {
+
+      this.hello = faker.fake('HeLLo {{name.lastName}}, {{name.firstName}} {{name.suffix}}');
+
+    }, 1500);
+
+  }
 }
